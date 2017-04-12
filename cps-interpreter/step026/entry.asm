cc008b43c318f9065623fe7c311d70736d9ade14
terry cadd <terry.cadd@gmail.com>
2017-04-12 16:06:11 +0100

    after hand cps conversion , try spill phase

--- cps-interpreter/step026/entry.asm
+++ cps-interpreter/step026/entry.asm
@@ -4,11 +4,15 @@ global scheme_entry
 scheme_entry: nop 
 mov dword esi , [ esp + 4 ] 
 scheme_heap_in_esi: nop
-mov dword eax , 40
-mov dword [ esp -4] , eax 
-mov dword eax , -40
-sub dword [ esp -4] , eax 
-mov dword eax , [ esp -4] 
+mov dword eax , 72
+mov dword [ esp -8] , eax ; save X 
+mov dword eax , 48
+mov dword [ esp -12] , eax ; save Y
+mov dword eax , 24
+mov dword [ esp -16] , eax ; save Z
+add dword esp , 0; adjust esp
+call tak ; -16
+sub dword esp , 0; restore esp
 ret
 
 
