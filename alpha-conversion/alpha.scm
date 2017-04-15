e1fda801b6142e3846eda6edae22d978d7dc1039
terry cadd <terry.cadd@gmail.com>
2017-04-15 22:39:02 +0100

    alpha conversion done

--- alpha-conversion/alpha.scm
+++ alpha-conversion/alpha.scm
@@ -7,12 +7,35 @@
 ;; each unique symbol is replaced by the same unique identifier as determined by scope
 ;; initially assuming environment is empty ? or doesnt it matter ?
 
-;; cont now takes 2 objects : <object> <new-environment>
-;; if 
+;; cont now takes 3 objects : object new-env new-gensyms 
+;; absolutely utterly brilliant use of continuation passing style
+
+
+;; primiitve language
+;; define , begin , set! 
+
+;; gensyms are ((a 1)(b 2)(c 1))
+;; so when we call ac-gensym
+;; we should also make a suitable entry in gensyms (gensyms) also 
+;;(assoc 'a '((a 1)(b 2)))
+;;(define (ac-update-gensym) #f)
+
+
+;; put a symbol and a integer and punt out a symbol , simples.
+(define (ac-gensym sym n)
+  (string->symbol (string-append (symbol->string sym)
+				 "_"
+				 (number->string n))))
+
 
 (define (ac-lambda? exp)
   (and (pair? exp)
        (eq? (car exp) 'lambda)))
+
+
+(define (ac-begin? exp)
+  (and (pair? exp)
+       (eq? (car exp) 'begin)))
 
 
 (define (ac-constant? exp)
@@ -24,27 +47,8 @@
 (define (ac-bound? exp env)
   (and (symbol? exp)
        (assoc exp env)))
-		 
 
-(define (make-unique sym)
-  (gensym (symbol->string sym)))
-  
 
-(define (ac exp env cont)
-  (cond
-   ;; if its a symbol , then it must be either bound OR toplevel
-   ((symbol? exp)
-    (if (ac-bound? exp env)
-	(begin
-	  (cont (cdr (assoc exp env)) env))
-	(begin (let ((gs (make-unique exp)))
-		 (cont gs (cons (cons exp gs) env))))))
-   ;; environment unchanged no binding to see here -- move on
-   ((ac-constant? exp) (cont exp env))
-   ((ac-lambda? exp) (ac-lambda exp env cont))
-   ((pair? exp) (ac-app exp env cont))
-   (else
-    (error "dont know how to process this " exp env cont))))
 
 
 (define (ac-extend-env keys vals env)
@@ -56,20 +60,100 @@
 			      (cons (car keys) (cons (car vals) '())))))))
 
 
+;; if given symbol is in the list , 
+
+
+
+;;  symbol -> gensyms  ->  \ a . b [ new-symbol  new-gensyms ] ....
+(define (ac-gensym-1 sym gens kab)
+  (let ((n (assoc sym gens)))
+    (if n
+	;; 
+	(begin
+	  (kab (ac-gensym sym (+ n 1))(cons (cons sym (+ n 1)) gens)))
+	;; 
+	(begin 
+	  (kab (ac-gensym sym 1) (cons (cons sym 1) gens))))))
+
+
+
+
+(define (ac exp env gens cont)
+  (cond   
+   ;; if its a symbol , then it must be either bound OR toplevel
+   ((symbol? exp)
+    (if (ac-bound? exp env)
+	(begin
+	  (cont (cdr (assoc exp env)) env gens))
+	(begin
+	  ;; (let ((gs (make-unique exp)))
+	  ;;   (cont gs
+	  ;; 	  (cons (cons exp gs) env)
+	  ;; 	  gens))
+	  (ac-gensym-1 exp gens (lambda (new-sym new-gensyms)
+				  (cont new-sym
+					(cons (cons exp new-sym) env)
+					new-gensyms))))))
+   
+   ;; environment unchanged no binding to see here -- move on
+   ((ac-constant? exp) (cont exp env gens))
+   ((ac-lambda? exp) (ac-lambda exp env gens cont))
+   ((ac-begin? exp) (ac-begin exp env gens cont))
+   ((pair? exp) (ac-app exp env gens cont))
+   (else
+    (error "dont know how to process this " exp env gens cont))))
+
+
+
+
+;; ensure binding is in order for lambda left to right
+;; for each binding , make a unique symbol
+;; then reverse them at end ...
+;; vars -> new-vars -> new-gensyms -> \ a . b  [ new-vars new-gensyms ]
+;;
+;; have to reverse the new-vars 
+(define (ac-lambda-vars vars new-vars new-env new-gensyms kab)
+  (cond
+   ((null? vars) (kab (reverse new-vars) new-env new-gensyms))
+   (else (let ((sym (car vars)))
+	   (ac-gensym-1 sym new-gensyms
+			(lambda (new-sym new-gens3)
+			  (ac-lambda-vars (cdr vars)
+					  (cons new-sym new-vars)
+					  (cons (cons sym new-sym) new-env)
+					  new-gens3
+					  kab)))))))
+
+
+
+
 ;; simple lambda lists
 ;; (lambda (a b c) ... )
-(define (ac-lambda exp env cont)
-  (let ((bindings (car (cdr exp)))
+(define (ac-lambda exp env gens cont)
+  (let ((vars (car (cdr exp)))
 	(body (cdr (cdr exp))))
-    (let ((new-bind (map (lambda (x) (cons x (make-unique x))) bindings)))
-      (let ((new-env (append new-bind env))
-	    (lambda-bind (map cdr new-bind)))
-	;; (display "new-env = ")
-	;; (display new-env)
-	;; (newline)
-	(ac-body body '() new-env (lambda (exp2 env2)
-				    ;; re-install the original environment env 
-				    (cont (list 'lambda_ lambda-bind exp2) env)))))))
+    (ac-lambda-vars vars
+		    '()
+		    '()
+		    gens
+		    (lambda (new-lambda-vars partial-env new-gensyms)
+		      ;; (let ((new-bind (map (lambda (x) (cons x (make-unique x))) bindings)))
+		      ;;   (let ((new-env (append new-bind env))
+		      ;;    (lambda-bind (map cdr new-bind)))
+		      ;; (display "new-env = ")
+		      ;; (display new-env)
+		      ;; (newline)
+		      (let ((new-env (append partial-env env)))
+			(ac-body body
+				 '()
+				 new-env
+				 new-gensyms
+				 (lambda (exp2 env2 gens3)
+				   ;; re-install the original environment env 
+				   (cont (append (list 'lambda_ new-lambda-vars)
+						 exp2)
+					 env
+					 gens3))))))))
 
 
 
@@ -81,30 +165,37 @@
 
 
 
-(define (ac-body body new-body env cont)
+
+
+(define (ac-body body new-body env gens cont)
   (cond
-   ((null? body) (cont new-body env))
+   ((null? body) (cont new-body env gens))
    (else
-    (ac (car body) env (lambda (body2 env2)
+    (ac (car body) env gens (lambda (body2 env2 gens2)
 			 (ac-body (cdr body)
 				  (append new-body (list body2))
 				  env2
+				  gens2
 				  cont))))))
 
 
 
+(define (ac-begin exp env gens cont)
+  (ac-body (cdr exp) '() env gens cont))
 
 
 
    
-(define (ac-app exp env cont)
-  (ac-body exp '() env cont))
+(define (ac-app exp env gens cont)
+  (ac-body exp '() env gens cont))
 		  
+
 
 (define (alpha-convert exp)
   (let ((env '())
-	(cont (lambda (exp2 env_) exp2)))
-    (ac exp env cont)))
+	(gens '())
+	(cont (lambda (exp2 env2 gens2) exp2)))
+    (ac exp env gens cont)))
 
 
 
@@ -146,14 +237,26 @@
 
 (define (test5)
   (alpha-convert '(lambda ()
-	       (lambda (x) x)
-	       (lambda (y) y)
-	       (lambda (z)
-		 (lambda (x)
-		   (lambda (y)
-		     z)
-		   x)
-		 z))))
+		    (lambda (x) x)
+		    (lambda (y) y)
+		    (lambda (z)
+		      (lambda (x)
+			(lambda (y)
+			  z)
+			x)
+		      z))))
+
+
+
+
+(define (test6)
+  (alpha-convert '(begin a b c )))
+
+
+(define (test7)
+  (alpha-convert '((define a (a b c))(define y (d e f a y))(define z (a b c))(cons 1 2))))
+
+
 
 
 
