c2c236525dc5aff99a043ef56510d5271641f9c4
terry cadd <terry.cadd@gmail.com>
2017-05-04 09:03:29 +0100

    works reasonable , need to figure out infrastructure to debug the code and build logs to see where stuff comes from

--- demo/queens/queens.scm
+++ demo/queens/queens.scm
@@ -4,29 +4,65 @@
 ;;
 
 
+(define (add1 n) (1+ n))
+(define (sub1 n) (1- n))
+
+
+(define (length xs)
+  (cond
+   ((null? xs) 0)
+   (else (+ 1 (length (cdr xs))))))
+
+
 ;_____________________________________________________
 ;This function tests to see if the next attempted move (try)
 ;is legal, given the list that has been constructed thus far
 ;(if any) - legal-pl (LEGAL PLacement list)
 ;N.B. - this function is an EXACT copy of the one from
 ;Springer and Friedman
+
+(define good?
+  (lambda (new-pl up down)
+    (cond
+     ((null? new-pl) #t)
+     (else (let ((next-pos (car new-pl)))
+	     (and
+	      (not (= next-pos try))
+	      (not (= next-pos up))
+	      (not (= next-pos down))
+	      (good? (cdr new-pl)
+		     (add1 up)
+		     (sub1 down))))))))
+
+
 (define legal?
   (lambda (try legal-pl)
-    (letrec
-        ((good?
-          (lambda (new-pl up down)
-            (cond
-              ((null? new-pl) #t)
-              (else (let ((next-pos (car new-pl)))
-                      (and
-                       (not (= next-pos try))
-                       (not (= next-pos up))
-                       (not (= next-pos down))
-                       (good? (cdr new-pl)
-                              (add1 up)
-                              (sub1 down)))))))))
-      (good? legal-pl (add1 try) (sub1 try)))))
-;_____________________________________________________
+    (good? legal-pl (add1 try) (sub1 try))))
+
+
+
+;; we dont do LETREC yet
+;;
+;; (define legal?
+;;   (lambda (try legal-pl)
+;;     (letrec
+;;         ((good?
+;;           (lambda (new-pl up down)
+;;             (cond
+;;               ((null? new-pl) #t)
+;;               (else (let ((next-pos (car new-pl)))
+;;                       (and
+;;                        (not (= next-pos try))
+;;                        (not (= next-pos up))
+;;                        (not (= next-pos down))
+;;                        (good? (cdr new-pl)
+;;                               (add1 up)
+;;                               (sub1 down)))))))))
+;;       (good? legal-pl (add1 try) (sub1 try)))))
+
+
+
+;;_____________________________________________________
 ;This function tests the length of the solution to
 ;see if we need to continue "cons"ing on more terms
 ;or not given to the specified board size.
@@ -77,15 +113,20 @@
 ;_____________________________________________________
 ;This is pretty much the same function as the one in the book
 ;with just my minor "boardsize" tweaks, since build-solution
-;is called.
+					;is called.
+
+(define build-loop
+  (lambda (boardsize sol)
+    (cond
+     ((null? sol) '())
+     (else (cons sol (build-loop boardsize (backtrack sol boardsize)))))))
+
+
 (define build-all-solutions
   (lambda (boardsize)
-    (letrec
-        ((loop (lambda (sol)
-                 (cond
-                   ((null? sol) '())
-                   (else (cons sol (loop (backtrack sol boardsize))))))))
-      (loop (build-solution '() boardsize)))))
+    (build-loop boardsize (build-solution '() boardsize))))
+
+
 ;_____________________________________________________
 ;This function I made up entirely myself, and I only
 ;made it really to satisfy the syntactical limitations
@@ -97,5 +138,6 @@
 (define queens
   (lambda (n)
     (build-all-solutions n)))
+
 
 
